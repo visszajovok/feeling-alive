1e863ff2df103731bae3125d80d77d4a83f7ec86
visszajovok <csendes.mira@gmail.com>
2026-03-24 14:55:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const verseny = new Date("2026-08-15 08:00:00").getTime();
+const verseny = new Date("2026-08-15").getTime();
 
 setInterval(() => {
   const most = new Date().getTime();
@@ -6,8 +6,7 @@ setInterval(() => {
 
   const nap = Math.floor(diff / (1000 * 60 * 60 * 24));
   const ora = Math.floor((diff / (1000 * 60 * 60)) % 24);
-  const perc = Math.floor((diff / (1000 * 60)) % 60);
 
   document.getElementById("ido").innerText =
-    nap + " nap " + ora + " óra " + perc + " perc";
+    nap + " nap " + ora + " óra";
 }, 1000);
